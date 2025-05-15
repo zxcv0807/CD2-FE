a3c911d30ff2b5c1db23ecd9b9bb525b20bf669d
zxcv0807 <pg8221@naver.com>
2025-05-15 21:06:29 +0900

    테스트

--- src/components/Oauth/GoogleCallbackPage.jsx
+++ src/components/Oauth/GoogleCallbackPage.jsx
@@ -1,37 +1,41 @@
 import { useEffect } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
-import axios from "../../api/axiosInstance";
+// import axios from "../../api/axiosInstance";
 import { useDispatch } from "react-redux";
 import { login } from "../../REDUX/auth/authSlice"
 
 const GoogleCallbackPage = () => {
   const [searchParams] = useSearchParams();
-  const code = searchParams.get("code");
-  const state = searchParams.get("state");
+  const access_token = searchParams.get("access_token");
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const handleGoogleCallback = async () => {
-      if (!code) return;
+    if (!access_token) return;
 
-      try {
-        const response = await axios.get("/api/v1/oauth/google/callback", {
-          withCredentials: true,
-          params: { code: code, state: state },
-        });
-        const { access_token, user_id } = response.data;
-        dispatch(login({ token: access_token, userId: user_id}));
+    dispatch(login({ token: access_token}));
+    navigate("/topics");
+    // const handleGoogleCallback = async () => {
+    //   if (!code) return;
 
-        navigate("/topics");
-      } catch (error) {
-        console.error("구글 로그인 콜백 처리 실패:", error);
-        navigate("/login");
-      }
-    };
+    //   try {
+    //     const response = await axios.get("/api/v1/oauth/google/callback", {
+    //       withCredentials: true,
+    //       params: { code: code, state: state },
+    //     });
+    //     const { access_token, user_id } = response.data;
+    //     dispatch(login({ token: access_token, userId: user_id}));
 
-    handleGoogleCallback();
-  }, [code, state, dispatch, navigate]);
+    //     navigate("/topics");
+    //   } catch (error) {
+    //     console.error("구글 로그인 콜백 처리 실패:", error);
+    //     navigate("/login");
+    //   }
+    // };
+
+    // handleGoogleCallback();
+  }, [access_token, dispatch, navigate]);
 
   return <div>구글 로그인 처리 중입니다...</div>;
 };
